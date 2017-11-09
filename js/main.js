41f91d53f8cad7cce21f23756c5b1bd0aa49a494
Jack <jacknashman@hotmail.co.uk>
2017-11-09 00:18:10 +0000

    Revert "test"
    This reverts commit df44f6c9e20ddcfca0a1102912bcb40512890366.

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() >= 20) {
+            $('.navbar').addClass('compressed');
+        } else {
+            $('.navbar').removeClass('compressed');
+        }
+    });
+	
+	$(window).on('scroll', function () {
+        if ($(window).scrollTop() >= 20) {
+            $('.header').addClass('fixed');
+        } else {
+            $('.header').removeClass('fixed');
+        }
+    });
+	
+	$(".scroll").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#b").offset().top -100 }, 2500);
+	});
+	
+	$("#ski").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#b").offset().top -100 }, 2500);
+	});
+	
+	$("#por").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#c").offset().top -100 }, 1750);
+	});
+	
+	$("#abo").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top -100 }, 1750);
+	});
+	
+	$("#contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#d").offset().top -100 }, 1250);
+	});
+
+});       
+
