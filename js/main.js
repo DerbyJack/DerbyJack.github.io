c68e41437f4dd9777a27968cfcbd9a2be78da9f7
DerbyJack <31997770+DerbyJack@users.noreply.github.com>
2018-02-06 15:26:40 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,8 +69,20 @@ $(document).ready(function () {
 	$("#insta").animate({marginTop: "10"}, 175);
 	});
 	
-	$(".selector img").click(function(){
-	$(".selector img").animate({opacity: "1.0"}, 175);
+	$("#sel1").click(function(){
+	$(this).animate({opacity: "1.0"}, 175);
+	});
+	
+	$("#sel2").click(function(){
+	$(this).animate({opacity: "1.0"}, 175);
+	});
+	
+	$("#sel3").click(function(){
+	$(this).animate({opacity: "1.0"}, 175);
+	});
+	
+	$("#sel4").click(function(){
+	$(this).animate({opacity: "1.0"}, 175);
 	});
 });       
 
